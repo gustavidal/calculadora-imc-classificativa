08622689ef2929ce3544f9f1dea0c5cb342f6af8
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2025-09-29 13:19:48 -0300

    Calculadora completa

--- src/br/senai/sp/jandira/imccondicional/model/Aluno.java
+++ src/br/senai/sp/jandira/imccondicional/model/Aluno.java
@@ -3,12 +3,60 @@ package br.senai.sp.jandira.imccondicional.model;
 public class Aluno {
 
     String nome;
-    String classificacao;
+    String status;
     int peso;
-    int altura;
     double imc;
+    double altura;
 
     public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Vamos começar!");
+        System.out.print("Primeiro, digite seu nome: ");
+        nome = leitor.nextLine();
+
+        System.out.print("Obrigado! Agora digite seu peso em kilogramas (kg): ");
+        peso = leitor.nextInt();
+
+        System.out.print("Quase lá. Só falta a sua altura! Pode digita-la em metros?: ");
+        altura = leitor.nextDouble();
+
+        calcularImc();
+    }
+
+    public void calcularImc(){
+        double alturaAoQuadrado = altura * altura;
+
+        imc = peso / alturaAoQuadrado;
+
+        classificarCondicao();
+    }
+
+    public void classificarCondicao(){
+        if (imc <= 18.5){
+            status = "ABAIXO DO PESO";
+        } else if (imc <= 24.9){
+            status = "PESO IDEAL";
+        } else if (imc <= 29.9){
+            status = "ACIMA DO PESO";
+        } else if (imc <= 34.9){
+            status = "OBESIDADE";
+        } else if (imc <= 39.9){
+            status = "OBESIDADE SEVERA";
+        } else {
+            status = "OBESIDADE MÓRBIDA";
+        }
+
+        exibirResultados();
+    }
+
+    public void exibirResultados(){
+        String imcDecimal = String.format("%.2f", imc);
+
+        System.out.println("-------------------------------------");
+        System.out.println("NOME: " + nome);
+        System.out.println("IMC: " + imcDecimal);
+        System.out.println("STATUS: " + status);
+        System.out.println("-------------------------------------");
     }
 }
